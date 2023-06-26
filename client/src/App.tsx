31562a88e47699166ea2b90b8a880cbeef83d70b
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-06-26 20:13:04 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import { sampleProducts } from "./data";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
