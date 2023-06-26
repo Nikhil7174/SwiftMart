5542a078407dd62b48f1eb3cec449befb3634d6c
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-06-27 00:35:11 +0530

    Added react router dom v6 and initialized home and products page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,36 @@
-import { useState } from "react";
+import { Col, Container, Nav, Navbar, Row } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import { sampleProducts } from "./data";
-import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>SwifMart</Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href="/cart" className="nav-link">
+              Cart
+            </a>
+            <a href="/signin" className="nav-link">
+              Sign In
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Outlet />
+        </Container>
+      </main>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App.tsx";
-import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+import "./index.css";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} element={<HomePage />} />
+      <Route path="product/:slug" element={<ProductPage />} />
+      {/* <Route path="dashboard" element={<Dashboard />} /> */}
+      {/* ... etc. */}
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
